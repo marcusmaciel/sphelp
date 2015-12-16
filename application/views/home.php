65a9c603742ea2ffefa587629386981fe4448ee8
darthsyom <marcusmacielwd@gmail.com>
2015-12-16 09:18:16 -0200

    Subindo a home
    home com pau

---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Dashboard</title>
+        <link href="<?php echo LAYOUT_CSS; ?>bootstrap.min.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+    </body>
+</html>
