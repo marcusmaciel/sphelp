1c2c767d16f41841c5d8e9d7413157a715a717de
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 09:44:27 -0200

    subindo as configurações novas...

--- application/controllers/Teste.php
+++ application/controllers/Teste.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Teste extends CI_Controller {
+class Page extends CI_Controller {
 
 	public function index()
 	{
-		echo 'ola mundo';
+		$this->load->view('welcome_message');
 	}
 }
