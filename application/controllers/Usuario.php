17b148051bc888a96aa8ef4f123150b3e322a619
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 22:35:38 -0200

    sistema de autenticação com verificação de status, login e senha funcionando 100%

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -2,15 +2,20 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Page extends CI_Controller {
+class Usuario extends CI_Controller {
 
-    private function autenticar() {
-        
-        $Usuario = $this->load->model('Usuario_model');
-        
-        echo $Usuario->getById();
-        
-        //print_r($this->input->post());
+    public function autenticar() {
+        //carrega o módulo de usuário para tentar autenticar o mesmo
+        $this->load->model('usuario_model', 'Usuario');
+
+        //verifica se algum usuário ativo é encontrado
+        $auth = $this->Usuario->getAutenticar($this->input->post('login'), $this->input->post('senha'));
+
+        if (count($auth) < 1){
+            echo 'usuário inválido!';
+        } else {
+            echo 'autenticou!!!!';
+        }
     }
 
 }
