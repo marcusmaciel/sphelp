1702d34aef949c3c191c36faa44998dd76f802eb
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 23:10:38 -0200

    resolvendo bug de sessões... as configurações do manual estão de acordo com o CI2, as sessões para dentro da base de dado mudaram no CI3

--- application/config/config.php
+++ application/config/config.php
@@ -367,15 +367,14 @@ $config['encryption_key'] = 'XFP9qnnPLbxcvrJfoNBr';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'sphelp_cookie';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'UsuarioSessao';
 $config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-$config['sess_table_name'] = 'UsuarioSessao';
-$config['sess_use_database'] = TRUE;
+
 
 /*
 |--------------------------------------------------------------------------
