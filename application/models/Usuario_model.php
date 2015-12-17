aabf3a9e1f3ee32235ce7763c7c6d0ad36c2d934
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 22:40:25 -0200

    criando funções
    criadas as funções para listar 1/todos os usuários e também para
    autenticar um usuário dentro do banco

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -2,6 +2,9 @@
 
 class Usuario_model extends CI_Model {
 
+    //tabela
+    var $table = 'Usuario';
+    //colunas
     var $_i = null;
     var $_s = '';
     var $_d = '';
@@ -13,11 +16,27 @@ class Usuario_model extends CI_Model {
     }
 
     public function getById($_i = null) {
-        return 'asdadsa';
+
+        //lista todos os usuários
+        if ($_i == null) {
+            $query = $this->db->get($this->table);
+        };
+
+        //lista somente o usuário que foi solicitado
+        if ($_i != null) {
+            $query = $this->db->get_where($this->table, array('_i' => $_i));
+        };
+
+        return $query->result();
     }
 
+    //autentica os usuários
     public function getAutenticar($login, $senha) {
         
+        //buscando usuários que estão ativos e que as informações listadas batem
+        $query = $this->db->get_where($this->table, array('_s' => 'ativo', 'login' => $login, 'senha' => $senha,));
+
+        return $query->result();
     }
 
     public function post($data) {
