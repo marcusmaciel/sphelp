dad7d678599ebb3ba5ae8ffeea23e44b154ee6d8
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-17 12:51:21 -0200

    criando estrutura de arquivos que são necessários para que o angular funcione junto ao requirejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,24 @@ define([
         'ui.router'
     ]);
 
+    $html = angular.element(document.getElementsByTagName('html')[0]);
+
+    angular.element().ready(function () {
+
+        try {
+
+            angular.bootstrap(document, [app.name]);
+
+        }
+
+        catch (e) {
+
+            console.error(e.stack || e.message || e);
+
+        }
+
+    });
+
     return app;
 
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-requirejs.config({
+require.config({
     paths: {
         //exports
         angular: '../angular/angular.min',
@@ -38,4 +38,35 @@ requirejs.config({
     deps: [
         'bootstrap'
     ]
+});
+
+require([
+    'angular',
+    'angularSanitize',
+    'jquery',
+    'uiRouter',
+    'config',
+    'values',
+    'constants/constants',
+    'filters/filters',
+    'services/services',
+    'directives/directives',
+    'controllers/controllers',
+], function (angular) {
+
+    'use strict';
+
+    var app = angular.module('app', [
+        'app.controllers',
+        'app.filters',
+        'app.services',
+        'app.directives',
+        'app.constants',
+        'app.config',
+        'app.values',
+        'ui.router'
+    ]);
+
+    return app;
+
 });
