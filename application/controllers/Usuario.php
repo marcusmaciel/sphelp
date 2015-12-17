463c33f1cc1f5a792b46bf4499babcb2697df014
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 23:42:34 -0200

    finalizada função de autenticar usuario (usando banco de dados) e também a de verificar se a sessão está expirada. caso esteja ele manda direto para a tela de login

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Usuario extends CI_Controller {
 
+    //verofoca se o login, senha estão certos e se esse usuário está ativo
     public function autenticar() {
         //carrega o módulo de usuário para tentar autenticar o mesmo
         $this->load->model('usuario_model', 'Usuario');
@@ -11,11 +12,21 @@ class Usuario extends CI_Controller {
         //verifica se algum usuário ativo é encontrado
         $auth = $this->Usuario->getAutenticar($this->input->post('login'), $this->input->post('senha'));
 
-        if (count($auth) < 1){
+        if (count($auth) < 1) {
             echo 'usuário inválido!';
         } else {
-            echo 'autenticou!!!!';
+            $this->session->set_userdata('Usuario', $auth);
+            print_r(json_encode($this->session->all_userdata()));
         }
+    }
+
+    public function sessao() {
+        //carrega a sessão instanciada (tenta)
+        $sessao = $this->session->userdata('user_id');
+        //se não houver o ID da sessão, envia o usuário para a página de login
+        if (!$sessao) {
+            $this->load->view('modulo/loginPage');
+        };
     }
 
 }
