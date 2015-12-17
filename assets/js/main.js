55a29c25b49d40504255a56056958ff1a5a020c3
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-17 11:30:30 -0200

    depois de muita sofrência, consegui fazer o requirejs ler e funcionar com os arquivos... agora já é correr para o processamento via angular huahuaeuaehuahuah

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,39 @@
-$(location).attr('href','google.com');
 requirejs.config({
     paths: {
         //exports
-        angular: '../lib/js/angular.min',
-        /**/angularAnimate: '../lib/js/angular-animate.min',
-        /**/angularSanitize: '../lib/js/angular-sanitize.min',
-        /**/uiRouter: '../lib/js/angular-ui-router.min',
-        jquery: '../lib/js/jquery-2.1.4.min',
+        angular: '../angular/angular.min',
+        /**/angularAnimate: '../angular/angular-animate.min',
+        /**/angularSanitize: '../angular/angular-sanitize.min',
+        /**/uiRouter: '../angular/angular-ui-router.min',
+        jquery: '../plugins/jQuery/jQuery-2.1.4.min',
+        /**/bootstrap: '../bootstrap/js/bootstrap.min',
+        /**/fastclick: '../plugins/fastclick/fastclick.min',
+        /**/adminLTE: '../dist/js/app.min',
+        /**/sparkline: '../plugins/sparkline/jquery.sparkline.min',
+        /**/jvectormap: '../plugins/jvectormap/jquery-jvectormap-1.2.2.min',
+        /**/jvectormap_worldMap: '../plugins/jvectormap/jquery-jvectormap-world-mill-en',
+        /**/slimScroll: '../plugins/slimScroll/jquery.slimscroll.min',
+        /**/chartjs: '../plugins/chartjs/Chart.min'
     },
     shim: {
         angular: {exports: 'angular'},
         /**/angularAnimate: {deps: ['angular']},
         /**/angularSanitize: {deps: ['angular']},
         /**/uiRouter: {deps: ['angular']},
-        spWebdb: {exports: 'spWebdb'},
         jquery: {exports: 'jquery'},
-        criptojs: {exports: 'criptojs'},
-        ionic: {deps: ['angular'], exports: 'ionic'},
-        /**/ionicAngular: {
-            deps: [
-                'angular',
-                'angularAnimate',
-                'angularSanitize',
-                'uiRouter',
-            ]
-        }
+        /**/bootstrap: {deps: ['jquery']},
+        /**/fastclick: {deps: ['jquery']},
+        /**/adminLTE: {deps: ['jquery']},
+        /**/sparkline: {deps: ['jquery']},
+        /**/jvectormap: {exports: 'jvectormap', deps: ['jquery']},
+        /**/jvectormap_worldMap: {deps: ['jquery', 'jvectormap']},
+        /**/slimScroll: {deps: ['jquery']},
+        /**/chartjs: {deps: ['jquery']}
     },
     priority: [
         'angular',
-        'ionic',
         'jquery',
-        'criptojs',
-        'spWebdb'
+        'jvectormap'
     ],
     deps: [
         'bootstrap'
