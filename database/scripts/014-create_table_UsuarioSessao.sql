7f0c6f8c814fd809bcd77a3680f8aac48793211d
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-16 23:43:09 -0200

    necessário criar a tabela de sessão dentro da base de dados para o CI poder gravá-la;

--- database/scripts/014-create_table_UsuarioSessao.sql
+++ database/scripts/014-create_table_UsuarioSessao.sql
@@ -1,11 +1,10 @@
 USE sphelp;
 /*create table*/
-CREATE TABLE IF NOT EXISTS  `sphelp`.`UsuarioSessao` (
-	session_id varchar(40) DEFAULT '0' NOT NULL,
-	ip_address varchar(45) DEFAULT '0' NOT NULL,
-	user_agent varchar(120) NOT NULL,
-	last_activity int(10) unsigned DEFAULT 0 NOT NULL,
-	user_data text NOT NULL,
-	PRIMARY KEY (session_id),
-	KEY `last_activity_idx` (`last_activity`)
+CREATE TABLE IF NOT EXISTS `sphelp`.`UsuarioSessao` (
+    `id` varchar(40) NOT NULL,
+    `ip_address` varchar(45) NOT NULL,
+    `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+    `data` blob NOT NULL,
+    PRIMARY KEY (id),
+    KEY `ci_sessions_timestamp` (`timestamp`)
 );
