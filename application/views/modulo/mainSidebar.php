7f51f0b26ba600193f45a803a5651643f196df36
marcusmaciel <marcus@marcusmaciel.com.br>
2015-12-18 14:38:17 -0200

    Subindo o menu
    menu menu menu

--- application/views/modulo/mainSidebar.php
+++ application/views/modulo/mainSidebar.php
@@ -4,19 +4,54 @@
     <section class="sidebar">
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
-            <li class="treeview">
+            <li>
                 <a href="#">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
                 </a>
             </li>
             <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-files-o"></i>
-                    <span>Layout Options</span>
+                    <i class="fa fa-life-ring"></i>
+                    <span>Central de Chamados</span>
                     <span class="label label-primary pull-right">4</span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-plus"></i> Novo chamado</a></li>
+                    <li><a href="pages/layout/boxed.html"><i class="fa fa-files-o"></i> Gerenciador</a></li>
+                    <li><a href="pages/layout/fixed.html"><i class="fa fa-tags"></i> Cad. Situação</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-group"></i>
+                    <span>Clientes</span>
+                    <span class="label label-primary pull-right">4</span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-plus"></i> Cadastro de clientes</a></li>
+                    <li><a href="pages/layout/boxed.html"><i class="fa fa-files-o"></i> Gerenciador</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-user"></i>
+                    <span>Usuários</span>
+                    <span class="label label-primary pull-right">4</span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-plus"></i> Cadastro de usuários</a></li>
+                    <li><a href="pages/layout/boxed.html"><i class="fa fa-file-archive-o"></i> Gerenciador</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-cog"></i>
+                    <span>Configurações</span>
+                    <span class="label label-primary pull-right">4</span>
+                </a>
+                <ul class="treeview-menu">
+                    
+                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Opção</a></li>
                     <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
                     <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
                     <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
