6993823741b7c4e163cfb0e2a2636f2b493251b9
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-18 14:03:39 -0200

    ae karaio ta funfando sapoha de http post do jeito certo agora aihiahiuaehuiauiha

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,13 @@ define(['angular'], function (angular) {
 
     return angular.module('app.config', [])
             .config(['$httpProvider', function ($httpProvider) {
-                    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+                    $httpProvider.defaults.transformRequest = function (data) {
+                        if (data === undefined) {
+                            return data;
+                        }
+                        return $.param(data);
+                    }
                 }])
             ;
 });
