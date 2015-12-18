fc6ccd54b827c25b33c1990aa6c32b3af36f6d9f
marcusmaciel <marcus@marcusmaciel.com.br>
2015-12-18 13:15:52 -0200

    Trabalhando na wrapper
    Trabalhando na wrapper

--- application/views/modulo/contentWrapper.php
+++ application/views/modulo/contentWrapper.php
@@ -3,8 +3,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            SP Help
-            <small>Version <strong>Alpha</strong></small>
+            Painel de chamados
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -16,7 +15,7 @@
     <section class="content">
         <div class="row">
             <div class="col-md-6 col-xs-12">
-                <div class="box box-primary">
+                <div class="box box-success">
                     <div class="box-header with-border">
                         <h3 class="box-title">Últimos chamados</h3>
                         <div class="box-tools pull-right">
@@ -27,48 +26,77 @@
                     <div class="box-body">
                         <ul class="products-list product-list-in-box">
                             <li class="item">
-                                <div class="product-img">
-                                    <img src="assets/dist/img/default-50x50.gif" alt="Product Image">
-                                </div>
-                                <div class="product-info">
-                                    <a href="javascript::;" class="product-title">Samsung TV <span class="label label-warning pull-right">$1800</span></a>
-                                    <span class="product-description">
-                                        Samsung 32" 1080p 60Hz LED Smart HDTV.
-                                    </span>
-                                </div>
+                                <a href="" class="item-chamado">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Empresa</label><br />
+                                            <span>System Port</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>CNPJ / CPF</label><br />
+                                            <span>00.000.000/0000-00</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Telefone</label><br />
+                                            <span>(31) 3333-2224</span>
+                                        </div>
+                                    </div>
+                                </a>
                             </li><!-- /.item -->
                             <li class="item">
-                                <div class="product-img">
-                                    <img src="assets/dist/img/default-50x50.gif" alt="Product Image">
-                                </div>
-                                <div class="product-info">
-                                    <a href="javascript::;" class="product-title">Bicycle <span class="label label-info pull-right">$700</span></a>
-                                    <span class="product-description">
-                                        26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-                                    </span>
-                                </div>
+                                <a href="" class="item-chamado">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Empresa</label><br />
+                                            <span>System Port</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>CNPJ / CPF</label><br />
+                                            <span>00.000.000/0000-00</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Telefone</label><br />
+                                            <span>(31) 3333-2224</span>
+                                        </div>
+                                    </div>
+                                </a>
                             </li><!-- /.item -->
                             <li class="item">
-                                <div class="product-img">
-                                    <img src="assets/dist/img/default-50x50.gif" alt="Product Image">
-                                </div>
-                                <div class="product-info">
-                                    <a href="javascript::;" class="product-title">Xbox One <span class="label label-danger pull-right">$350</span></a>
-                                    <span class="product-description">
-                                        asdasd
-                                    </span>
-                                </div>
+                                <a href="" class="item-chamado">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Empresa</label><br />
+                                            <span>System Port</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>CNPJ / CPF</label><br />
+                                            <span>00.000.000/0000-00</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Telefone</label><br />
+                                            <span>(31) 3333-2224</span>
+                                        </div>
+                                    </div>
+                                </a>
                             </li><!-- /.item -->
+                            
                             <li class="item">
-                                <div class="product-img">
-                                    <img src="assets/dist/img/default-50x50.gif" alt="Product Image">
-                                </div>
-                                <div class="product-info">
-                                    <a href="javascript::;" class="product-title">PlayStation 4 <span class="label label-success pull-right">$399</span></a>
-                                    <span class="product-description">
-                                        PlayStation 4 500GB Console (PS4)
-                                    </span>
-                                </div>
+                                <a href="" class="item-chamado">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label>Empresa</label><br />
+                                            <span>System Port</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>CNPJ / CPF</label><br />
+                                            <span>00.000.000/0000-00</span>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label>Telefone</label><br />
+                                            <span>(31) 3333-2224</span>
+                                        </div>
+                                    </div>
+                                </a>
                             </li><!-- /.item -->
                         </ul>
                     </div><!-- /.box-body -->
@@ -76,8 +104,6 @@
                         <a href="javascript::;" class="uppercase">Todos os chamados</a>
                     </div><!-- /.box-footer -->
                 </div><!-- /.box -->
-            </div>
-            <div class="col-md-6 col-xs-12">
                 <div class="row">
                     <div class="col-md-6 col-sm-6 col-xs-12">
                         <div class="info-box">
@@ -85,7 +111,7 @@
 
                             <div class="info-box-content">
                                 <span class="info-box-text">Chamados em aberto</span>
-                                <span class="info-box-number">41,410</span>
+                                <span class="info-box-number">520</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -104,6 +130,9 @@
                         <!-- /.info-box -->
                     </div>
                 </div>
+            </div>
+            <div class="col-md-6 col-xs-12">
+
             </div>
         </div>
     </section><!-- /.content -->
