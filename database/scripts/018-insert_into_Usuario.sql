e86b29c77643e2aa0735e9c6aadb6dd004928208
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-19 17:13:27 -0200

    adicionando uma coluna para inserir o nome do usuário. o login não fica usual, ele sempre será o index mas o nome do usuário pode ser o mesmo sim

--- database/scripts/018-insert_into_Usuario.sql
+++ database/scripts/018-insert_into_Usuario.sql
@@ -1,3 +1,3 @@
 USE sphelp;
 /*inserts*/
-INSERT INTO `sphelp`.`Usuario` (`login`, `senha`) SELECT * FROM (SELECT 'admin', '123') AS tmp WHERE NOT EXISTS (SELECT `login` FROM `sphelp`.`Usuario` WHERE `login` = 'admin') LIMIT 1;
+INSERT INTO `sphelp`.`Usuario` (`login`, `senha`, `nome`) SELECT * FROM (SELECT 'admin', '123', 'Super User') AS tmp WHERE NOT EXISTS (SELECT `login` FROM `sphelp`.`Usuario` WHERE `login` = 'admin') LIMIT 1;
