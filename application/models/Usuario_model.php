ceec0da0a4af444486ce2fbfd9bfd51f1fe3a449
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-19 11:45:56 -0200

    finalizando todas os métodos para fazer o CRUD do usuário

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -48,17 +48,49 @@ class Usuario_model extends CI_Model {
 
     //cadastra um novo usuário
     public function post($data) {
-        
+        $this->db->insert($this->table, $data);
+        return $this->insert_id();
     }
 
     //atualiza um usuário
     public function put($data) {
-        
+        //define o $index de update
+        $this->db->where($data->_i);
+        //exlui elementos que não devem atualizar
+        $values = $data;
+        unset($values[array_search('_i', $values)]);
+        unset($values[array_search('_d', $values)]);
+        //check para atualizar e retorna status
+        if ($this->db->update($this->table, $values)) {
+            return true;
+        } else {
+            return false;
+        };
     }
 
-    //deleta (oculta um usuário)
+    //deleta (oculta um usuário se houver movimentação)
     public function delete($_i) {
-        
+        //array de verificação
+        $check = array();
+        //tabelas que serão analisadas
+        $tables = array('Atendimento', 'Chamado', 'Historico');
+        //pra cada tabela, veja se há registros deste _i
+        foreach ($tables as $table) {
+            $this->db->where('Usuario_i', $_i);
+            $query = $this->db->count_all($tables[$table]);
+            if ($query != 0) {
+                $check[$tables[$table]] = $query;
+            }
+        }
+        //se não houver registros delete o usuário
+        if (count($check) == 0) {
+            $this->db->delete($this->table, array('_i' => $_i));
+            return true;
+        }
+        //caso contrário retorne as tabelas e a quantidade de registros
+        else {
+            return $check;
+        }
     }
 
 }
