5e42aa43ce53ef6c2b7262a6d27f6c07b7c5d709
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-19 15:39:44 -0200

    colocando ionicons localmente
    isso irá melhorar a velocidade de carregamento de dados a partir do fato
    que não será necessário buscar isso em outro servidor

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,12 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session','database','upload');
+$autoload['libraries'] = array(
+    'session',
+    'database',
+    'upload',
+    'email'
+    );
 
 /*
 | -------------------------------------------------------------------
@@ -83,7 +88,11 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url',
+    'file',
+    'email'
+    );
 
 /*
 | -------------------------------------------------------------------
