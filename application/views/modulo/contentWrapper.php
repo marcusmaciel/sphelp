3cc6c4df4fb401a9b083a4d71a7f53e772390627
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-18 23:23:47 -0200

    passado o content fixo de chamados em aberto para a direita, assim quando a tela estiver menor os chamados ficarão abaixo do conteúdo dinamico

--- application/views/modulo/contentWrapper.php
+++ application/views/modulo/contentWrapper.php
@@ -1,137 +1,94 @@
 <!--modulo/mainHeader-->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Painel de chamados
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    </section>
 
-    <!-- Main content -->
+    <!-- content -->
     <section class="content">
+
         <div class="row">
-            <div class="col-md-6 col-xs-12">
+            
+            <!--área dinamica (manipulável sobre as ações do menu)-->
+            <div class="col-lg-6 col-md-4 col-sm-12 col-xs-12">
                 <div class="box box-success">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Últimos chamados</h3>
-                        <div class="box-tools pull-right">
-                            <button class="btn btn-box-tool" data-widget="refresh"><i class="fa fa-refresh"></i></button>
-                        </div>
-                    </div><!-- /.box-header -->
+                        <h3 class="box-title">
+                            Estatísticas
+                        </h3>
+                    </div>
+                    <!-- /.box-header -->
                     <div class="box-body">
-                        <ul class="products-list product-list-in-box">
-                            <li class="item">
-                                <a href="" class="item-chamado">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <label>Empresa</label><br />
-                                            <span>System Port</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>CNPJ / CPF</label><br />
-                                            <span>00.000.000/0000-00</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>Telefone</label><br />
-                                            <span>(31) 3333-2224</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li><!-- /.item -->
-                            <li class="item">
-                                <a href="" class="item-chamado">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <label>Empresa</label><br />
-                                            <span>System Port</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>CNPJ / CPF</label><br />
-                                            <span>00.000.000/0000-00</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>Telefone</label><br />
-                                            <span>(31) 3333-2224</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li><!-- /.item -->
-                            <li class="item">
-                                <a href="" class="item-chamado">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <label>Empresa</label><br />
-                                            <span>System Port</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>CNPJ / CPF</label><br />
-                                            <span>00.000.000/0000-00</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>Telefone</label><br />
-                                            <span>(31) 3333-2224</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li><!-- /.item -->
-                            
-                            <li class="item">
-                                <a href="" class="item-chamado">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <label>Empresa</label><br />
-                                            <span>System Port</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>CNPJ / CPF</label><br />
-                                            <span>00.000.000/0000-00</span>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label>Telefone</label><br />
-                                            <span>(31) 3333-2224</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li><!-- /.item -->
-                        </ul>
-                    </div><!-- /.box-body -->
-                    <div class="box-footer text-center">
-                        <a href="javascript::;" class="uppercase">Todos os chamados</a>
-                    </div><!-- /.box-footer -->
+                        adadsadasas
+                    </div>
+                    <!-- /.box-body -->
                 </div><!-- /.box -->
-                <div class="row">
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-orange"><i class="fa fa-life-ring"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text">Chamados em aberto</span>
-                                <span class="info-box-number">520</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-green"><i class="fa fa-thumbs-up"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text">Finalizados hoje</span>
-                                <span class="info-box-number">41,410</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                </div>
             </div>
-            <div class="col-md-6 col-xs-12">
-
+            
+            <!--chamados em aberto (fixa em todo o sistema)-->
+            <div class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">
+                            Chamados em aberto
+                            <span class="badge bg-green">300</span>
+                        </h3>
+                        <div class="box-tools">
+                            <button type="button" class="btn btn-box-tool">
+                                <i class="fa fa-refresh"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table class="table table-bordered">
+                            <tbody><tr>
+                                    <th style="width: 40px">Id</th>
+                                    <th>Empresa</th>
+                                    <th>Descrição</th>
+                                    <th style="width: 40px">Data/Hora</th>
+                                </tr>
+                                <tr>
+                                    <td>1.</td>
+                                    <td>Update software</td>
+                                    <td>
+                                        <div class="progress progress-xs">
+                                            <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                                        </div>
+                                    </td>
+                                    <td><span class="badge bg-red">55%</span></td>
+                                </tr>
+                                <tr>
+                                    <td>2.</td>
+                                    <td>Clean database</td>
+                                    <td>
+                                        <div class="progress progress-xs">
+                                            <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
+                                        </div>
+                                    </td>
+                                    <td><span class="badge bg-yellow">70%</span></td>
+                                </tr>
+                                <tr>
+                                    <td>3.</td>
+                                    <td>Cron job running</td>
+                                    <td>
+                                        <div class="progress progress-xs progress-striped active">
+                                            <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
+                                        </div>
+                                    </td>
+                                    <td><span class="badge bg-light-blue">30%</span></td>
+                                </tr>
+                                <tr>
+                                    <td>4.</td>
+                                    <td>Fix and squish bugs</td>
+                                    <td>
+                                        <div class="progress progress-xs progress-striped active">
+                                            <div class="progress-bar progress-bar-success" style="width: 90%"></div>
+                                        </div>
+                                    </td>
+                                    <td><span class="badge bg-green">90%</span></td>
+                                </tr>
+                            </tbody></table>
+                    </div>
+                    <!-- /.box-body -->
+                </div><!-- /.box -->
             </div>
         </div>
     </section><!-- /.content -->
