c2cae842a89a01b418cbecaa5aadea4d8d59984b
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-19 10:27:23 -0200

    criando modelo para retorno da tabela TipoContato

--- application/models/TipoContato_model.php
+++ application/models/TipoContato_model.php
@@ -2,6 +2,9 @@
 
 class TipoContato_model extends CI_Model {
 
+    //tabela
+    var $table = 'TipoContato';
+    //colunas
     var $_i = null;
     var $descricao = '';
 
@@ -9,20 +12,8 @@ class TipoContato_model extends CI_Model {
         parent::__construct();
     }
 
-    public function getById($_i = null) {
-        
-    }
-
-    public function post($data) {
-        
-    }
-
-    public function put($data) {
-        
-    }
-
-    public function delete($_i) {
-        
+    public function get() {
+        return $this->db->get($this->table)->return();
     }
 
 }
