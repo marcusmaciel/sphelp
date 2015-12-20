6fdd778cceb349dbcaeb880ebf170daa434d18c1
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 15:29:43 -0200

    finalizando crud de ClasseContato

--- application/models/ClasseContato_model.php
+++ application/models/ClasseContato_model.php
@@ -7,13 +7,77 @@ class ClasseContato_model extends CI_Model {
     //colunas
     var $_i = null;
     var $descricao = '';
+    //colunas não alteráveis
+    var $column_blocks = array('_i');
 
     function __construct() {
         parent::__construct();
     }
 
-    public function get() {
-        return $this->db->get($this->table)->return();
+    //busca pelo _i (1 ou todos)
+    public function get($_i = null) {
+
+        if ($_i != null) {
+            $this->db->where('_i', $_i);
+        };
+
+        return $this->db->get($this->table)->result();
+    }
+
+    public function post($data) {
+
+        //insere na tabeça
+        $this->db->insert($this->table, $data);
+
+        return $this->insert_id();
+    }
+
+    public function put($data) {
+
+        //define o $index de update
+        $this->db->where($data->_i);
+
+        //exlui elementos que não devem atualizar
+        $values = $data;
+        foreach ($column_blocks as $column) {
+            unset($values[array_search($column_blocks[$column], $values)]);
+        };
+
+        //check para atualizar e retorna status
+        if ($this->db->update($this->table, $values)) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+
+    public function delete($_i) {
+
+        //array de verificação
+        $check = array();
+
+        //tabelas que serão analisadas
+        $tables = array('ClienteContato');
+
+        //pra cada tabela, veja se há registros deste _i
+        foreach ($tables as $table) {
+            $this->db->where('ClasseContato_i', $_i);
+            $query = $this->db->count_all($tables[$table]);
+            if ($query != 0) {
+                $check[$tables[$table]] = $query;
+            }
+        }
+
+        //se não houver registros delete
+        if (count($check) == 0) {
+            $this->db->delete($this->table, array('_i' => $_i));
+            return true;
+        }
+
+        //caso contrário retorne as tabelas e a quantidade de registros
+        else {
+            return $check;
+        }
     }
 
 }
