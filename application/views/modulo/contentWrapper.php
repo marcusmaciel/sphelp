268ac830e33a4c0594cfb840e4bd96fa006e5f98
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 15:21:15 -0200

    inseri as classes para que as colunas de descrição do problema e também de SLA sejam ocultadas quando estiver no mobile

--- application/views/modulo/contentWrapper.php
+++ application/views/modulo/contentWrapper.php
@@ -39,52 +39,54 @@
                     <!-- /.box-header -->
                     <div class="box-body" id="info-chamados">
                         <table class="table table-condensed table-hover table-fixed">
-                            <tbody>
+                            <thead>
                                 <tr>
                                     <th class="text-center" style="width: 40px;">#</th>
-                                    <th style="width:35%">Empresa</th>
-                                    <th>Descrição</th>
-                                    <th style="width: 70px">SLA</th>
+                                    <th>Empresa</th>
+                                    <th class="hidden-xs">Descrição</th>
+                                    <th class="hidden-xs" style="width: 70px">SLA</th>
                                 </tr>
+                            </thead>
+                            <tbody>
                                 <tr tabindex="-1">
                                     <td class="text-center"><a href="#">1</a></td>
                                     <td>Update software</td>
-                                    <td class="cover">
+                                    <td class="cover hidden-xs">
                                         impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. 
                                         Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, 
                                         permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a
                                     </td>
-                                    <td><span class="badge">aberto</span></td>
+                                    <td class="hidden-xs"><span class="badge">aberto</span></td>
                                 </tr>
                                 <tr tabindex="-1">
                                     <td class="text-center"><a href="#">2</a></td>
                                     <td>Clean database</td>
-                                    <td class="cover">
+                                    <td class="cover hidden-xs">
                                         ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem 
                                         de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do texto. 
                                         Todos os geradores de Lorem Ipsum na internet tendem a repetir pedaços predefinidos conforme necessário.
                                     </td>
-                                    <td><span class="badge">aberto</span></td>
+                                    <td class="hidden-xs"><span class="badge">aberto</span></td>
                                 </tr>
                                 <tr>
                                     <td class="text-center"><a href="#">3</a></td>
                                     <td>Cron job running</td>
-                                    <td class="cover">
+                                    <td class="cover hidden-xs">
                                         impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. 
                                         Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, 
                                         permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a
                                     </td>
-                                    <td><span class="badge">aberto</span></td>
+                                    <td class="hidden-xs"><span class="badge">aberto</span></td>
                                 </tr>
                                 <tr>
                                     <td class="text-center"><a href="#">4</a></td>
                                     <td>Fix and squish bugs</td>
-                                    <td class="cover">
+                                    <td class="cover hidden-xs">
                                         impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. 
                                         Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, 
                                         permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a
                                     </td>
-                                    <td><span class="badge">aberto</span></td>
+                                    <td class="hidden-xs"><span class="badge">aberto</span></td>
                                 </tr>
                             </tbody>
                         </table>
