1ff8cd3b64eaac10e125038e21b78a34229e4587
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 15:55:54 -0200

    finalizado o CRUD de Historico. ele não contém o put porque o histórico não deve ser editado. além disso também criei uma nova forma de fazer seleções no banco de dados, mais simples e limpa

--- application/models/Historico_model.php
+++ application/models/Historico_model.php
@@ -2,47 +2,53 @@
 
 class Historico_model extends CI_Model {
 
+    //tabela
+    var $table = 'Historico';
+    //colunas
     var $_i = null;
     var $_s = '';
     var $_d = '';
     var $Usuario_i = null;
     var $acao = '';
     var $conteudo = '';
+    //colunas não alteráveis
+    var $column_blocks = array('_i', '_d');
 
     function __construct() {
         parent::__construct();
     }
 
-    public function getById($_i = null) {
-        
+    //busca usando as informações enviadas
+    public function get($data = array()) {
+
+        foreach ($data as $key => $value) {
+            $this->db->where($key, $value);
+        }
+
+        return $this->db->get($this->table)->result();
     }
 
-    public function getByData($dataIni,$dataEnd) {
-        
-    }
-
-    public function getByUsuario($Usuario_i) {
-        
-    }
-
-    public function getByAcao($acao) {
-        
-    }
-
-    public function getByValor($valor) {
-        
-    }
-
+    //cadastra um novo historico
     public function post($data) {
-        
+
+        //insere na tabeça
+        $this->db->insert($this->table, $data);
+
+        return $this->insert_id();
     }
 
-    public function put($data) {
-        
-    }
+    //public function put($data) {} #histórico não deve ser atualizado
+    
+    //remove registros com base nos filtros enviados
+    public function delete($data = array()) {
 
-    public function delete($_i) {
-        
+        foreach ($data as $key => $value) {
+            $this->db->where($key, $value);
+        }
+
+        $this->db->delete($this->table);
+
+        return $this->db->affected_rows();
     }
 
 }
