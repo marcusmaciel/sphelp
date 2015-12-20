dfd788b5b50370103c1e3cc2c19ac7765a56ef36
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 16:18:30 -0200

    atualizando referencias do endereço e contato do cliente. estes cadastros devem ser removidos caso eu remova um cliente do sistema

--- database/scripts/009-create_table_ClienteEndereco.sql
+++ database/scripts/009-create_table_ClienteEndereco.sql
@@ -1,21 +1,21 @@
 USE sphelp;
 /*create table*/
 CREATE TABLE IF NOT EXISTS `sphelp`.`ClienteEndereco` (
-  `_i` INT NOT NULL AUTO_INCREMENT,
-  `_s` VARCHAR(50) NOT NULL DEFAULT 'ativo',
-  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Cliente_i` INT NOT NULL,
-  `TipoEndereco_i` INT NOT NULL,
-  `cep` CHAR(8) NOT NULL,
-  `logradouro` VARCHAR(100) NOT NULL,
-  `complemento` VARCHAR(150) NULL DEFAULT '00000000000',
-  `bairro` VARCHAR(50) NOT NULL,
-  `localidade` VARCHAR(50) NOT NULL,
-  `uf` CHAR(2) NOT NULL,
-  PRIMARY KEY (`_i`),
-  INDEX `FK_Endereco$TipoEndereco_i_idx` (`TipoEndereco_i` ASC),
-  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC),
-  INDEX `FK_ClienteEndereco$Cliente_i_idx` (`Cliente_i` ASC),
+  `_i` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `_s` VARCHAR(50) NOT NULL DEFAULT 'ativo' COMMENT '',
+  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `Cliente_i` INT NOT NULL COMMENT '',
+  `TipoEndereco_i` INT NOT NULL COMMENT '',
+  `cep` CHAR(8) NOT NULL COMMENT '',
+  `logradouro` VARCHAR(100) NOT NULL COMMENT '',
+  `complemento` VARCHAR(150) NULL DEFAULT '00000000000' COMMENT '',
+  `bairro` VARCHAR(50) NOT NULL COMMENT '',
+  `localidade` VARCHAR(50) NOT NULL COMMENT '',
+  `uf` CHAR(2) NOT NULL COMMENT '',
+  PRIMARY KEY (`_i`)  COMMENT '',
+  INDEX `FK_Endereco$TipoEndereco_i_idx` (`TipoEndereco_i` ASC)  COMMENT '',
+  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC)  COMMENT '',
+  INDEX `FK_ClienteEndereco$Cliente_i_idx` (`Cliente_i` ASC)  COMMENT '',
   CONSTRAINT `FK_ClienteEndereco$TipoEndereco_i`
     FOREIGN KEY (`TipoEndereco_i`)
     REFERENCES `sphelp`.`TipoEndereco` (`_i`)
@@ -24,6 +24,6 @@ CREATE TABLE IF NOT EXISTS `sphelp`.`ClienteEndereco` (
   CONSTRAINT `FK_ClienteEndereco$Cliente_i`
     FOREIGN KEY (`Cliente_i`)
     REFERENCES `sphelp`.`Cliente` (`_i`)
-    ON DELETE RESTRICT
+    ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
--- database/scripts/010-create_table_ClienteContato.sql
+++ database/scripts/010-create_table_ClienteContato.sql
@@ -1,19 +1,19 @@
 USE sphelp;
 /*create table*/
 CREATE TABLE IF NOT EXISTS `sphelp`.`ClienteContato` (
-  `_i` INT NOT NULL AUTO_INCREMENT,
-  `_s` VARCHAR(50) NOT NULL DEFAULT 'ativo',
-  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Cliente_i` INT NOT NULL,
-  `TipoContato_i` INT NOT NULL,
-  `ClasseContato_i` INT NOT NULL,
-  `descricao` VARCHAR(100) NOT NULL,
-  `observacao` LONGTEXT NULL,
-  PRIMARY KEY (`_i`),
-  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC),
-  INDEX `FK_ClienteContato$TipoContato_i_idx` (`TipoContato_i` ASC),
-  INDEX `FK_ClienteContato$ClasseContato_i_idx` (`ClasseContato_i` ASC),
-  INDEX `FK_ClienteContato$Cliente_i_idx` (`Cliente_i` ASC),
+  `_i` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `_s` VARCHAR(50) NOT NULL DEFAULT 'ativo' COMMENT '',
+  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `Cliente_i` INT NOT NULL COMMENT '',
+  `TipoContato_i` INT NOT NULL COMMENT '',
+  `ClasseContato_i` INT NOT NULL COMMENT '',
+  `descricao` VARCHAR(100) NOT NULL COMMENT '',
+  `observacao` LONGTEXT NULL COMMENT '',
+  PRIMARY KEY (`_i`)  COMMENT '',
+  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC)  COMMENT '',
+  INDEX `FK_ClienteContato$TipoContato_i_idx` (`TipoContato_i` ASC)  COMMENT '',
+  INDEX `FK_ClienteContato$ClasseContato_i_idx` (`ClasseContato_i` ASC)  COMMENT '',
+  INDEX `FK_ClienteContato$Cliente_i_idx` (`Cliente_i` ASC)  COMMENT '',
   CONSTRAINT `FK_ClienteContato$TipoContato_i`
     FOREIGN KEY (`TipoContato_i`)
     REFERENCES `sphelp`.`TipoContato` (`_i`)
@@ -27,6 +27,6 @@ CREATE TABLE IF NOT EXISTS `sphelp`.`ClienteContato` (
   CONSTRAINT `FK_ClienteContato$Cliente_i`
     FOREIGN KEY (`Cliente_i`)
     REFERENCES `sphelp`.`Cliente` (`_i`)
-    ON DELETE RESTRICT
+    ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
