0cce441f98d02e4121d7b3d8855d16eabc9ac7c4
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 21:36:14 -0200

    finalizado a forma para listar os chamados;

---
+++ application/controllers/Chamado.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Chamado extends CI_Controller {
+
+    //retorna no formato json
+    private function toJson($data) {
+        $this->output
+                ->set_content_type('application/json; charset=utf-8')
+                ->set_output(json_encode($data));
+    }
+
+    //seleciona um ou todos de acordo com a o parametro e o texto de pesquisa
+    public function listar() {
+
+        //carrega o módulo de usuário para tentar autenticar o mesmo
+        $this->load->model('Chamado_model', 'Chamado');
+
+        $result = $this->Chamado->get($this->input->post());
+
+        $this->toJson($result);
+    }
+
+}
