61cf43d1fa8ec2d43ea89fb787bede8b0225d995
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 21:46:34 -0200

    remoção do menu de chamados. a box dele vai estar sempre presente, portanto não há necessidade de ter um menu. também dei uma limpada no que temos de menu

--- application/views/modulo/mainSidebar.php
+++ application/views/modulo/mainSidebar.php
@@ -1,41 +1,38 @@
 <!--modulo/mainSidebar-->
 <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
+
     <section class="sidebar">
-        <!-- sidebar menu: : style can be found in sidebar.less -->
+
         <ul class="sidebar-menu">
-            <li>
-                <a href="" ng-click="configuracoes()">
+            <li name="configuracoes">
+                <a href>
                     <i class="fa fa-cogs"></i> 
                     <span>Configurações</span>
                 </a>
-            </li>
-            <li>
-                <a href="#">
+            </li><!--/.[name="configuracoes"]-->
+            <li name="clientes">
+                <a href>
                     <i class="fa fa-users"></i> 
                     <span>Clientes</span>
                 </a>
-            </li>
-            <li>
-                <a href="#">
+            </li><!--/.[name="clientes"]-->
+
+            <li name="usuarios">
+                <a href>
                     <i class="fa fa-mouse-pointer"></i> 
                     <span>Usuarios</span>
                 </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-life-ring"></i> 
-                    <span>Chamados</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
+            </li><!--/.[name="usuarios"]-->
+
+            <li name="relatorios">
+                <a href>
                     <i class="fa fa-files-o"></i> 
                     <span>Relatórios</span>
                 </a>
-            </li>
+            </li><!--/.[name="relatorios"]-->
 
-        </ul>
-    </section>
-    <!-- /.sidebar -->
+        </ul><!--/.sidebar-menu-->
+        
+    </section><!-- /.sidebar -->
+    
 </aside>
