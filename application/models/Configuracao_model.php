344534339562eaa9a555eb9e74516d1795c50a15
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-20 16:12:38 -0200

    finalizado CRUD de configurações; inserções ou remoções são feitas somente através de atualizações do sistema.

--- application/models/Configuracao_model.php
+++ application/models/Configuracao_model.php
@@ -2,43 +2,54 @@
 
 class Configuracao_model extends CI_Model {
 
+    //tabela
+    var $table = 'Configuracao';
+    //colunas
     var $_i = null;
-    var $_s = '';
-    var $_d = '';
     var $descricao = '';
     var $chave = '';
     var $valor = '';
+    //colunas não alteráveis
+    var $column_blocks = array('_i', 'chave');
 
     function __construct() {
         parent::__construct();
     }
 
-    public function getById($_i = null) {
-        
+    //busca usando as informações enviadas
+    public function get($data = array()) {
+
+        foreach ($data as $key => $value) {
+            $this->db->where($key, $value);
+        }
+
+        return $this->db->get($this->table)->result();
     }
 
-    public function getByDescricao($descricao) {
-        
-    }
-
-    public function getByChave($chave) {
-        
-    }
-
-    public function getByValor($valor) {
-        
-    }
-
-    public function post($data) {
-        
-    }
-
+    //novo registro
+    //public function post($data) {} # somente inserido através de atualizações
+    
+    //atualiza um usuário
     public function put($data) {
-        
+
+        //define o $index de update
+        $this->db->where('_i', $data->_i);
+
+        //exlui elementos que não devem atualizar
+        $values = $data;
+        foreach ($column_blocks as $column) {
+            unset($values[array_search($column_blocks[$column], $values)]);
+        };
+
+        //check para atualizar e retorna status
+        if ($this->db->update($this->table, $values)) {
+            return true;
+        } else {
+            return false;
+        };
     }
 
-    public function delete($_i) {
-        
-    }
+    //deleta
+    //public function delete($_i) {} # somente removida através de atualizações
 
 }
