af277e599d0b80b05da5ef9b7b5581eff5deb786
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-21 08:05:34 -0200

    corrigindo tabela Versão (script)

--- database/scripts/019-create_table_Versao.sql
+++ database/scripts/019-create_table_Versao.sql
@@ -1,11 +1,11 @@
 ﻿USE sphelp;
 /*create table*/
 CREATE TABLE IF NOT EXISTS `sphelp`.`Versao` (
-  `_i` INT NOT NULL COMMENT '',
-  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
-  `numero` VARCHAR(50) NOT NULL COMMENT '',
-  `implementacao` LONGTEXT NULL COMMENT '',
-  `correcao` LONGTEXT NULL COMMENT '',
-  PRIMARY KEY (`_i`)  COMMENT '',
-  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC)  COMMENT '')
+  `_i` INT NOT NULL AUTO_INCREMENT,
+  `_d` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `numero` VARCHAR(50) NOT NULL,
+  `implementacao` LONGTEXT NULL,
+  `correcao` LONGTEXT NULL,
+  PRIMARY KEY (`_i`),
+  UNIQUE INDEX `_i_UNIQUE` (`_i` ASC))
 ENGINE = InnoDB
