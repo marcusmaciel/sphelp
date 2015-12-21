cbabadd17def2d5c6a23f68288905c307bbd68c8
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-21 14:04:51 -0200

    agora o content irá carregar somente o que for definido no início. e ele irá separar também por colunas

--- application/views/modulo/contentWrapper.php
+++ application/views/modulo/contentWrapper.php
@@ -6,27 +6,13 @@
 
         <div class="row">
 
-            <!--área dinamica-->
-            <div name="leftCol" class="col-lg-6 col-md-4 col-sm-12 col-xs-12">
-                <?php
-                if (isset($leftCol)) {
-                    foreach ($leftCol as $componente => $html) {
-                        echo $html;
-                    };
+            <?php
+            if (isset($content)) {
+                foreach ($content as $col => $html) {
+                    echo $html;
                 };
-                ?>
-            </div>
-
-            <!--chamados em aberto (fixa em todo o sistema)-->
-            <div name="rightCol" class="col-lg-6 col-md-8 col-sm-12 col-xs-12">
-                <?php
-                if (isset($rightCol)) {
-                    foreach ($rightCol as $componente => $html) {
-                        echo $html;
-                    };
-                };
-                ?>
-            </div>
+            };
+            ?>
 
         </div>
     </section><!-- /.content -->
