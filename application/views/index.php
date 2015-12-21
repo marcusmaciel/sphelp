e9b6f86d7f38ddb26d045b34a031ec9636bdd93a
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-21 14:02:09 -0200

    estava fazendo testes para que o sidebar funcionasse usando a lista de chamados em aberto

--- application/views/index.php
+++ application/views/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
     <?php if ($head) echo $head; ?>
-    <body class="hold-transition skin-green sidebar-mini sidebar-collapse control-sidebar-open">
+    <body class="hold-transition skin-green sidebar-mini sidebar-collapse">
         <div class="wrapper">
             <?php if (isset($mainHeader)) echo $mainHeader; ?>
             <?php if (isset($mainSidebar)) echo $mainSidebar; ?>
             <?php if (isset($contentWrapper)) echo $contentWrapper; ?>
             <?php if (isset($mainFooter)) echo $mainFooter; ?>
+            <?php if (isset($controlSidebar)) echo $controlSidebar; ?>
             <?php if (isset($loginPage)) echo $loginPage; ?>
         </div>
     </body>
