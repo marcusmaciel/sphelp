5f9584cbff7b0efe0a65f7a26890d05d1c733940
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-21 16:18:16 -0200

    reduzindo o nome da systemport para melhor visualização no mobile

--- application/views/modulo/mainFooter.php
+++ application/views/modulo/mainFooter.php
@@ -5,6 +5,7 @@
     </div>
     <strong>
         <span class="hidden-xs">Copyright &copy; 2000-2015 </span>
-        <a href="http://systemcommerce.net">System Port Technology</a>
-    </strong><span class="hidden-xs"> - Todos os direitos reservados.</span>
+        <a href="http://systemcommerce.net">System Port</a>
+    </strong>
+    <span class="hidden-xs"> - Todos os direitos reservados.</span>
 </footer>
