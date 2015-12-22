f893d966a9c97be23248d71bc33ddf7bb4ec58ab
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-22 12:12:33 -0200

    colocando para que quando uma tarefa estiver em foco (mouseover) o angular limpe o numero de tarefas da barra do usuário

--- application/views/modulo/controlSidebar.php
+++ application/views/modulo/controlSidebar.php
@@ -1,25 +1,31 @@
 <!--modulo/controlSidebar-->
-<aside class="control-sidebar control-sidebar-dark">
-    <div name="chamados-em-aberto" class="tab-content" ng-controller="controlSidebarController">
+<aside class="control-sidebar control-sidebar-dark" ng-controller="controlSidebarController">
+    <div name="chamados-em-aberto" class="tab-content">
         <h3 class="control-sidebar-heading">
             <span>Chamados em aberto</span>
             <span class="badge bg-green" ng-if="listaChamados.length > 0">{{listaChamados.length}}</span>
         </h3>
         <ul class="control-sidebar-menu">
-            
+
             <li class="overlay" ng-if="loading == true">
                 <i class="fa fa-refresh fa-spin"></i>
             </li><!-- /.overlay -->
 
-            <li ng-repeat="chamado in listaChamados">
+            <!--chamados-->
+            <li ng-repeat="chamado in listaChamados"  ng-mouseover="limpaNovosChamados()">
                 <span class="badge bg-blue">{{chamado._i}}</span>
                 <div class="menu-info" tooltip-placement="left"
-                       data-toggle="tooltip" uib-tooltip="{{chamado.descricao}}">
+                     data-toggle="tooltip" uib-tooltip="{{chamado.descricao}}">
                     <h4 class="control-sidebar-subheading sp-text-overflow-ellipsis">{{chamado.descricao}}</h4>
                     <p>{{chamado.sla}}</p>
                 </div>
             </li>
-            
+
+            <!--nenhum chamado-->
+            <li ng-if="listaChamados.length == 0" style="justify-content: center;">
+                <h4 class="control-sidebar-subheading sp-text-overflow-ellipsis">nenhum chamados em aberto!</h4>
+            </li>
+
         </ul><!-- /.control-sidebar-menu -->
     </div><!--/.[name=chamados-em-aberto]-->
 </aside>
