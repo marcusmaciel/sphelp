7161cc8da490ae16e444737dcf50b9d3da2d3279
Leandro Santiago Gomes <leandroluk@gmail.com>
2015-12-23 23:44:18 -0200

    testando loading de modal

--- application/views/contentWrapper/perfilInfo.php
+++ application/views/contentWrapper/perfilInfo.php
@@ -1,12 +1,24 @@
-<div class="box box-default">
-    <div class="box-header with-border">
-        <h3 class="box-title">
-            Perfil do Usuario
-        </h3>
+<div class="info-box" ng-controller="perfilInfoController">
+    <a href tooltip-placement="bottom" uib-tooltip="Ver meu perfil" ng-click="modalOpen()"
+       class="info-box-icon bg-yellow animate-icon" ng-class="">
+        <i class="ion ion-android-alert hidden" ng-class="{show: msgWarning.length != ''}"></i>
+        <i class="ion ion-android-happy hidden" ng-class="{show: msgWarning.length == ''}"></i>
+    </a>
+
+    <div class="info-box-content">
+        <span class="info-box-text">Olá <?php if (isset($nome)) echo $nome; ?></span>
+        <a href tooltip-placement="bottom" uib-tooltip="Ver chamados" class="info-box-number hidden" 
+           ng-class="{show: msgWarning.length != ''}">
+            Você tem 1000 precisam de atenção.
+        </a>
+        <span class="info-box-number hidden" ng-class="{show: msgWarning.length == ''}">
+            Não há chamados em alerta.
+        </span>
     </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        adadsadasas
-    </div>
-    <!-- /.box-body -->
-</div><!-- /.box -->
+
+    <div class="overlay" ng-class="{hidden : loading == false, show : loading == true}">
+        <i class="fa fa-refresh fa-spin"></i>
+    </div><!-- /.overlay -->
+    
+    <!-- /.info-box-content -->
+</div>
